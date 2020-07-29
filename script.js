8649b0ab5465266c875b53d787bf3f4d2c6589da
Valeria Curseri <curserivaleria@gmail.com>
2020-07-29 16:06:22 +0200

    Conversazione OK ma non funziona il DELAY del TIMEOUT

--- script.js
+++ script.js
@@ -1,49 +1,42 @@
 // Milestone 2
-// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e, cliccando invia (l'icona o anche con enter), il testo viene aggiunto al thread sopra, come messaggio verde.
-// Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
-// Suggerimento: Utilizzate il template come vi ho fatto vedere a lezione, ma soprattutto state attenti a trovare l'elemento giusto dove inserire (appendere) i valori ricavati dal campo input.
-// In poche parole se il mio template prevede un tag p (possibilmente con una determinata classe) dove inserire del testo, devo trovarlo e poi aggiungere il testo!
-// Ricordatevi anche che alla fine il clone va pushato (incollato) dove desiderate.
 
-// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e, cliccando invia (l'icona o anche con enter), il testo viene aggiunto al thread sopra, come messaggio verde.
+// A. Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e, cliccando invia (l'icona o anche con enter), il testo viene aggiunto al thread sopra, come messaggio verde.
 
-// 1. all'invio dopo aver digitato nell'input #messaggio, succede qualcosa (fz)
+// B. Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 
-$('#messaggio').keyup(nuovoMessaggio);
-
-// 2. scrivo la fz per generare un nuovo messaggio
+var nuovoMessaggio = $('#messaggio').keyup(nuovoMessaggio);
 
 function nuovoMessaggio(){
-    if(event.keyCode == 13 || event.which == 13){
-        // 2.1 memorizzo il valore di #messaggio
-        var contenutoMessaggio = $(this).val();
-        var cloneMessaggio = $('#template-messaggio .riga-messaggio').clone();
-        // 2.3 appendo in p il valore memorizzato
-        cloneMessaggio.find('.testo-messaggio').append(contenutoMessaggio);
-        // 2.4 aggiungo la classe .inviato per personalizzare il css
-        cloneMessaggio.find('.messaggio').addClass('inviato');
-        // 4.1 aggiungo l'ora di invio
-        cloneMessaggio.find('.ora-messaggio').append(oraInvio());
-        // 2.5 appendo il messaggio creato in #conversazione
-        $('#conversazione').append(cloneMessaggio);
+    if(event.keyCode == 13 || event.which == 13){       // se il tasto rilasciato è INVIO
+        var contenutoMessaggio = $(this).val();         // memorizzo il valore di input
+        nuovo(contenutoMessaggio,'inviato');            // genero il mio messaggio con la fz nuovo
+        setTimeout(nuovo('Bella!','ricevuto'),30000);   // genero la risposta automatica con la fz nuovo - NOTA: NON FUNZIONA IL DELAY
     }
 }
 
-// 3. scrivo la fz per aggiungere uno 0 se l'ora o i minuti hanno una sola cifra
+// ***** funzioni ***** //
 
+// fz per aggiungere uno 0 se l'ora o i minuti hanno una sola cifra
 function aggiungiZero(i){
-    if(i < 10){
-        return '0' + i;
+    if(i < 10){                                     // se i è minore di 10 (ha una sola cifra)
+        return '0' + i;                             // aggiungi uno 0 davanti
     }
     return i;
 }
 
-// 4. scrivo la fz per generare l'ora
+// fz per generare l'ora
 function oraInvio(){
-    var ora = new Date();
-    var h = aggiungiZero(ora.getHours());
-    var m = aggiungiZero(ora.getMinutes());
+    var ora = new Date();                           // creo l'oggetto
+    var h = aggiungiZero(ora.getHours());           // cerco l'ora nell'oggetto - se necessario aggiungo 0 davanti
+    var m = aggiungiZero(ora.getMinutes());         // cerco i minuti nell'oggetto - se necessario aggiungo 0 davanti
     return h + ':' + m;
 }
 
-// Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+// fz per generare un nuovo messaggio (contenuto e classe da definire)
+function nuovo(contenuto,classeCss){
+    var cloneTemplateMessaggio = $('#template-messaggio .riga-messaggio').clone();  // clono il template del messaggio
+    cloneTemplateMessaggio.find('.testo-messaggio').append(contenuto);              // appendo in p il contenuto
+    cloneTemplateMessaggio.find('.messaggio').addClass(classeCss);                  // aggiungo la classeCss per personalizzare il css
+    cloneTemplateMessaggio.find('.ora-messaggio').append(oraInvio());               // aggiungo l'ora di invio
+    $('#conversazione').append(cloneTemplateMessaggio);                             // appendo il messaggio creato in #conversazione
+}
