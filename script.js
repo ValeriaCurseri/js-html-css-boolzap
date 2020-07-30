3d906fe69158a5ec1a58265730d5731e9d4fda17
Valeria Curseri <curserivaleria@gmail.com>
2020-07-30 12:13:29 +0200

    Layout e .active migliorati. aeroplanino sistemato

--- script.js
+++ script.js
@@ -30,8 +30,9 @@ function nuovoMessaggio(){
             var messaggioRisposta = nuovo('Bella!','ricevuto');
             setTimeout(messaggioRisposta,30000);                        // genero la risposta automatica con la fz nuovo - NOTA: NON FUNZIONA IL DELAY
             $('#messaggio').val('');                                    // pulisco il campo di input
-            $('.conversazione.active').scrollTop(2000);                 // scrollo per vedere subito i nuovi messaggi
+            $('.scambio.active .conversazione').scrollTop(2000);                 // scrollo per vedere subito i nuovi messaggi
         }
+        invio = false;
     }
 }
 
@@ -55,9 +56,9 @@ function oraInvio(){
 
 // fz per generare un nuovo messaggio (contenuto e classe da definire)
 function nuovo(contenuto,classeCss){
-    var cloneTemplateMessaggio = $('.conversazione.active #template-messaggio .riga-messaggio').clone();  // clono il template del messaggio
+    var cloneTemplateMessaggio = $('.scambio.active #template-messaggio .riga-messaggio').clone();  // clono il template del messaggio
     cloneTemplateMessaggio.find('.testo-messaggio').append(contenuto);              // appendo in p il contenuto
     cloneTemplateMessaggio.find('.messaggio').addClass(classeCss);                  // aggiungo la classeCss per personalizzare il css
     cloneTemplateMessaggio.find('.ora-messaggio').append(oraInvio());               // aggiungo l'ora di invio
-    $('.conversazione.active').append(cloneTemplateMessaggio);                             // appendo il messaggio creato in .conversazione
+    $('.scambio.active .conversazione').append(cloneTemplateMessaggio);                             // appendo il messaggio creato in .conversazione
 }
