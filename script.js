72e27b1ef4ed05f1cd65949e251563533db60d0f
Valeria Curseri <curserivaleria@gmail.com>
2020-08-23 17:35:09 +0200

    Ricerca funzionante

--- script.js
+++ script.js
@@ -1,41 +1,58 @@
-// Milestone 2
+$(document).ready(function(){
 
-// A. Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e, cliccando invia (l'icona o anche con enter), il testo viene aggiunto al thread sopra, come messaggio verde.
+    // mostro l'aeroplanino invece del microfono al click su input #messaggio
+    $('#messaggio').click(function(){
+        $('#strumenti .fa-microphone').addClass('display-none');
+        $('#strumenti .fa-paper-plane').removeClass('display-none');
+    });
 
-// B. Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+    // all'invio e al click sull'aeroplanino invio il messaggio e genero una risposta automatica
 
-// mostro l'aeroplanino invece del microfono al click su input #messaggio
-$('#messaggio').click(function(){
-    $('#strumenti .fa-microphone').addClass('display-none');
-    $('#strumenti .fa-paper-plane').removeClass('display-none');
-});
+    var invio = false;                                                      // variabile sentinella utile per inviare al click
 
-// all'invio e al click sull'aeroplanino invio il messaggio e genero una risposta automatica
+    $('#strumenti .fa-paper-plane').click(function(){
+        invio = true;
+    })
 
-var invio = false;                                                      // variabile sentinella utile per inviare al click
+    $('#messaggio').keyup(nuovoMessaggio);
 
-$('#strumenti .fa-paper-plane').click(function(){
-    invio = true;
+    $('#strumenti .fa-paper-plane').click(nuovoMessaggio);
+
+    // RICERCA: ricerco il valore dell'input nella colonna #cerca-contatti all'interno degli span con classe .nome della colonna #cronologia-chat. quello che trovo rimane. se non trovo niente scrivo 'La ricerca non ha prodotto alcun risultato'
+
+    $('#cerca-contatti input').keyup(function(event){                           // quando i tasti si sollevano compilando l'input
+        var valoreRicerca = $('#cerca-contatti input').val().toLowerCase();     // memorizzo il valore dell'input
+        console.log(valoreRicerca);         // DA CANCELLARE
+        $('#cronologia-chat .nome').each(function(){                            // per ogni chat presente nella mia cronologia
+            var nomeChat = $(this).text().toLowerCase();
+            if (nomeChat.includes(valoreRicerca) == false){         // se il nome del mio contatto non contiene il valore dell'input
+                $(this).closest('.chat').css('display', 'none');    // non lo mostro
+            } else {                                                // se il nome del mio contatto contiene il valore dell'input
+                $(this).closest('.chat').css('display', 'flex');   // lo mostro
+            }
+        })
+    })
+
+    // CAROUSEL: cliccando sulle diverse chat nella colonna #cronologia-chat, nella colonna di destra apparirà la chat selezionata e i dettagli del contatto selezionato
+
+
 })
 
-$('#messaggio').keyup(nuovoMessaggio);
+// ***** funzioni ***** //
 
-$('#strumenti .fa-paper-plane').click(nuovoMessaggio);
-
-function nuovoMessaggio(){
+function nuovoMessaggio(){  // fz per inviare un messaggio
     if(event.keyCode == 13 || event.which == 13 || invio == true){      // se il tasto rilasciato è INVIO
         var contenutoMessaggio = $('#messaggio').val();                 // memorizzo il valore di input
+        console.log(contenutoMessaggio);
         if (contenutoMessaggio != ''){                                  // se il contenuto dell'input non è vuoto
             nuovo(contenutoMessaggio,'inviato');                        // genero il mio messaggio con la fz nuovo
-            setTimeout(nuovo,3000,'Bella!','ricevuto');                 // genero la risposta automatica con la fz nuovo - NOTA: NON FUNZIONA IL DELAY
+            setTimeout(nuovo,3000,'Bella!','ricevuto');                 // genero la risposta automatica con la fz nuovo
             $('#messaggio').val('');                                    // pulisco il campo di input
             $('.scambio.active .conversazione').scrollTop(2000);        // scrollo per vedere subito i nuovi messaggi
         }
         invio = false;
     }
 }
-
-// ***** funzioni ***** //
 
 // fz per aggiungere uno 0 se l'ora o i minuti hanno una sola cifra
 function aggiungiZero(i){
@@ -61,3 +78,6 @@ function nuovo(contenuto,classeCss){
     cloneTemplateMessaggio.find('.ora-messaggio').append(oraInvio());                               // aggiungo l'ora di invio
     $('.scambio.active .conversazione').append(cloneTemplateMessaggio);                             // appendo il messaggio creato in .conversazione
 }
+
+// NOTE:
+// scrollTop non funziona benissimo
