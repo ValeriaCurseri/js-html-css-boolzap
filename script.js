91d77e32ac00351a03a83f18e212d8e98bc46d26
Valeria Curseri <curserivaleria@gmail.com>
2020-08-23 18:50:04 +0200

    click sulle diverse chat e sostituzione ora e nome su header della conv

--- script.js
+++ script.js
@@ -33,8 +33,24 @@ $(document).ready(function(){
         })
     })
 
-    // CAROUSEL: cliccando sulle diverse chat nella colonna #cronologia-chat, nella colonna di destra apparirà la chat selezionata e i dettagli del contatto selezionato
+    // CAROUSEL: cliccando sulle diverse chat nella colonna #cronologia-chat, nella colonna di destra apparirà la chat selezionata (A) e i dettagli del contatto selezionato
 
+    $('#cronologia-chat .chat').click(function(){                                   // 1. al click sulle singole chat
+        $('#cronologia-chat .chat.active').removeClass('active');                   // A2. rimuovo la classe active dalla chat che la possiede
+        $(this).addClass('active');                                                 // A3. la chat cliccata ottiene la class active
+        var chatActiveIndex = $(this).index();                                      // A4. individuo l'index della chat cliccata
+        var conversazioneActive = $('#chat .conversazione').eq(chatActiveIndex);    // A5. individuo la conversazione con lo stesso index del pallino cliccato
+        $('#chat .conversazione.active').removeClass('active');                     // A6. rimuovo la classe active dalla conversazione che la possiede
+        conversazioneActive.addClass('active');                                     // A7. do alla conversazione con lo stesso indice della chat cliccata la class active
+        var nome = $('#cronologia-chat .chat.active .nome').text();                 // B8. memorizzo il nome della chat cliccata
+        $('#chat .header .nome').text(nome);                                        // B9. lo inserisco nell'header con .text()
+        var ora = $('#cronologia-chat .chat.active .ultimo-accesso').text();        // B10. memorizzo l'ora dell'ultima connessione della chat cliccata
+        $('#chat .header .ultimo-accesso').text(ora);                               // B11. la inserisco nell'header con .text()
+        var img = $('#cronologia-chat .chat.active img').attr('src');    // memorizzo l'attributo src dell'immagine della chat cliccata
+        $('#chat .header .img').attr('src', img);    // lo sostituisco all'attributo search già presente nell'header
+
+
+    });
 
 })
 
@@ -48,7 +64,7 @@ function nuovoMessaggio(){  // fz per inviare un messaggio
             nuovo(contenutoMessaggio,'inviato');                        // genero il mio messaggio con la fz nuovo
             setTimeout(nuovo,3000,'Bella!','ricevuto');                 // genero la risposta automatica con la fz nuovo
             $('#messaggio').val('');                                    // pulisco il campo di input
-            $('.scambio.active .conversazione').scrollTop(2000);        // scrollo per vedere subito i nuovi messaggi
+            $('.conversazione.active').scrollTop(2000);        // scrollo per vedere subito i nuovi messaggi
         }
         invio = false;
     }
@@ -72,11 +88,11 @@ function oraInvio(){
 
 // fz per generare un nuovo messaggio (contenuto e classe da definire)
 function nuovo(contenuto,classeCss){
-    var cloneTemplateMessaggio = $('.scambio.active #template-messaggio .riga-messaggio').clone();  // clono il template del messaggio
+    var cloneTemplateMessaggio = $('.conversazione.active #template-messaggio .riga-messaggio').clone();  // clono il template del messaggio
     cloneTemplateMessaggio.find('.testo-messaggio').append(contenuto);                              // appendo in p il contenuto
     cloneTemplateMessaggio.find('.messaggio').addClass(classeCss);                                  // aggiungo la classeCss per personalizzare il css
     cloneTemplateMessaggio.find('.ora-messaggio').append(oraInvio());                               // aggiungo l'ora di invio
-    $('.scambio.active .conversazione').append(cloneTemplateMessaggio);                             // appendo il messaggio creato in .conversazione
+    $('.conversazione.active').append(cloneTemplateMessaggio);                             // appendo il messaggio creato in .conversazione
 }
 
 // NOTE:
